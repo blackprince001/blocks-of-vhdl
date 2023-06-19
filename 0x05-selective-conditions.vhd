f4509aa68fae77d87fe4b354c5ceb9bd46d7e3c9
blackprince001 <appiahboaduprince@gmail.com>
2023-06-19 07:26:29 +0000

    Add Selective Conditions lesson

---
+++ 0x05-selective-conditions.vhd
@@ -0,0 +1,17 @@
+-- what is the use if you are only giving me linting
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity ent is
+    port (
+        clk : in std_logic;
+        rst : in std_logic;
+        sig : out std_logic
+    );
+end ent;
+
+architecture rtl of ent is
+    
+begin
+
+end architecture;
